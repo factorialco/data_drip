fd3bb45cd561e9a8df51f4a9b0a82a89ce0ec853
Oriol Gual <oriol.gual@factorial.co>
2026-02-06 08:40:50 +0100

    Add before and around hooks

--- app/models/data_drip/backfill_run_batch.rb
+++ app/models/data_drip/backfill_run_batch.rb
@@ -2,6 +2,8 @@
 
 module DataDrip
   class BackfillRunBatch < ApplicationRecord
+    include DataDrip::Hookable
+
     self.table_name = "data_drip_backfill_run_batches"
 
     belongs_to :backfill_run, class_name: "DataDrip::BackfillRun"
@@ -16,49 +18,79 @@ module DataDrip
       %i[pending enqueued running completed failed stopped]
     )
 
+    def pending!(*args, &block)
+      with_action_hooks(:pending) { super(*args, &block) }
+    end
+
+    def enqueued!(*args, &block)
+      with_action_hooks(:enqueued) { super(*args, &block) }
+    end
+
+    def running!(*args, &block)
+      with_action_hooks(:running) { super(*args, &block) }
+    end
+
+    def completed!(*args, &block)
+      with_action_hooks(:completed) { super(*args, &block) }
+    end
+
+    def failed!(*args, &block)
+      with_action_hooks(:failed) { super(*args, &block) }
+    end
+
+    def stopped!(*args, &block)
+      with_action_hooks(:stopped) { super(*args, &block) }
+    end
+
     after_commit :enqueue, on: :create
-    after_commit :run_hooks
+    after_commit :run_status_change_hooks
 
     def enqueue
       return unless pending?
 
-      DataDrip::DripperChild.perform_later(self)
-      enqueued!
+      with_action_hooks(:enqueued) do
+        DataDrip::DripperChild.perform_later(self)
+        enqueued!
+      end
     end
 
     def run!
-      running!
-      migration =
-        backfill_run.backfill_class.new(
-          batch_size: batch_size,
-          sleep_time: 5,
-          backfill_options: backfill_run.options
-        )
+      with_action_hooks(:running) do
+        running!
+        migration =
+          backfill_run.backfill_class.new(
+            batch_size: batch_size,
+            sleep_time: 5,
+            backfill_options: backfill_run.options
+          )
 
-      migration
-        .scope
-        .in_batches(
-          of: batch_size,
-          start: start_id,
-          finish: finish_id
-        ) do |batch|
-          migration.send(:process_batch, batch)
-          sleep 5
-        end
+        migration
+          .scope
+          .in_batches(
+            of: batch_size,
+            start: start_id,
+            finish: finish_id
+          ) do |batch|
+            migration.send(:process_batch, batch)
+            sleep 5
+          end
+      end
     end
 
     private
 
-    def run_hooks
-      return unless status_previously_changed?
+    def hook_target_for(hook_name)
+      backfill_class = backfill_run.backfill_class
+      return backfill_class if backfill_class.respond_to?(hook_name)
 
-      hook_name = "on_batch_#{status}"
-      if backfill_run.backfill_class.respond_to?(hook_name)
-        backfill_run.backfill_class.send(hook_name, self)
-      elsif DataDrip.hooks_handler_class.present? &&
-            DataDrip.hooks_handler_class.respond_to?(hook_name)
-        DataDrip.hooks_handler_class.send(hook_name, self)
-      end
+      handler = DataDrip.hooks_handler_class
+      return handler if handler&.respond_to?(hook_name)
+
+      nil
+    end
+
+    def hook_prefix
+      "batch"
     end
   end
 end
--- spec/test_app/app/services/hook_handler.rb
+++ spec/test_app/app/services/hook_handler.rb
@@ -1,17 +1,58 @@
 class HookHandler
-  def self.on_run_completed(run)
-    puts "on_run_completed"
+  def self.before_run_enqueued(run)
+    sequence = HookNotifier.instance.get("handler_run_enqueued_sequence") || []
+    sequence << "before"
+    HookNotifier.instance.set("handler_run_enqueued_sequence", sequence)
+    HookNotifier.instance.set("handler_before_run_enqueued", run.id)
   end
 
-  def self.on_run_enqueued(run)
-    puts "on_run_enqueued"
+  def self.around_run_enqueued(run)
+    sequence = HookNotifier.instance.get("handler_run_enqueued_sequence") || []
+    sequence << "around_before"
+    HookNotifier.instance.set("handler_run_enqueued_sequence", sequence)
+    yield
+    sequence = HookNotifier.instance.get("handler_run_enqueued_sequence") || []
+    sequence << "around_after"
+    HookNotifier.instance.set("handler_run_enqueued_sequence", sequence)
   end
 
-  def self.on_batch_enqueued(batch)
-    puts "on_batch_enqueued"
+  def self.after_run_enqueued(run)
+    sequence = HookNotifier.instance.get("handler_run_enqueued_sequence") || []
+    sequence << "after"
+    HookNotifier.instance.set("handler_run_enqueued_sequence", sequence)
   end
 
-  def self.on_batch_completed(batch)
-    puts "on_batch_completed"
+  def self.after_run_completed(run)
+    HookNotifier.instance.set("handler_after_run_completed", run.id)
+  end
+
+  def self.after_batch_enqueued(batch)
+    HookNotifier.instance.set("handler_after_batch_enqueued", batch.id)
+  end
+
+  def self.after_batch_completed(batch)
+    HookNotifier.instance.set("handler_after_batch_completed", batch.id)
+  end
+
+  def self.before_batch_running(batch)
+    sequence = HookNotifier.instance.get("handler_batch_running_sequence") || []
+    sequence << "before"
+    HookNotifier.instance.set("handler_batch_running_sequence", sequence)
+  end
+
+  def self.around_batch_running(batch)
+    sequence = HookNotifier.instance.get("handler_batch_running_sequence") || []
+    sequence << "around_before"
+    HookNotifier.instance.set("handler_batch_running_sequence", sequence)
+    yield
+    sequence = HookNotifier.instance.get("handler_batch_running_sequence") || []
+    sequence << "around_after"
+    HookNotifier.instance.set("handler_batch_running_sequence", sequence)
+  end
+
+  def self.after_batch_running(batch)
+    sequence = HookNotifier.instance.get("handler_batch_running_sequence") || []
+    sequence << "after"
+    HookNotifier.instance.set("handler_batch_running_sequence", sequence)
   end
 end
