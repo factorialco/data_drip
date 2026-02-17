64fc7ecc73e04f425e07f21ee2ed3c2106ee2cef
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2026-02-17 09:23:41 +0200

    Restore schema from main

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -10,50 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_20_142547) do
+ActiveRecord::Schema[7.0].define(version: 2025_11_20_142547) do
   create_table "data_drip_backfill_run_batches", force: :cascade do |t|
     t.bigint "backfill_run_id", null: false
-    t.integer "batch_size", default: 100, null: false
-    t.datetime "created_at", null: false
-    t.text "error_message"
-    t.bigint "finish_id", null: false
-    t.bigint "start_id", null: false
     t.integer "status", default: 0, null: false
+    t.text "error_message"
+    t.integer "batch_size", default: 100, null: false
+    t.bigint "start_id", null: false
+    t.bigint "finish_id", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["backfill_run_id"], name: "index_data_drip_backfill_run_batches_on_backfill_run_id"
-    t.index ["status"], name: "index_data_drip_backfill_run_batches_on_status"
+    t.index [ "backfill_run_id" ],
+            name: "index_data_drip_backfill_run_batches_on_backfill_run_id"
+    t.index [ "status" ], name: "index_data_drip_backfill_run_batches_on_status"
   end
 
   create_table "data_drip_backfill_runs", force: :cascade do |t|
-    t.integer "amount_of_elements"
     t.string "backfill_class_name", null: false
-    t.bigint "backfiller_id", null: false
-    t.integer "batch_size", default: 100, null: false
-    t.datetime "created_at", null: false
-    t.text "error_message"
     t.json "options", default: {}, null: false
-    t.integer "processed_count", default: 0, null: false
-    t.datetime "start_at", null: false
+    t.text "error_message"
     t.integer "status", default: 0, null: false
+    t.integer "batch_size", default: 100, null: false
     t.integer "total_count"
+    t.integer "processed_count", default: 0, null: false
+    t.integer "amount_of_elements"
+    t.bigint "backfiller_id", null: false
+    t.datetime "start_at", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["status"], name: "index_data_drip_backfill_runs_on_status"
+    t.index [ "status" ], name: "index_data_drip_backfill_runs_on_status"
   end
 
   create_table "employees", force: :cascade do |t|
+    t.string "name"
     t.integer "age"
+    t.string "role"
     t.date "birthday"
     t.datetime "created_at", null: false
-    t.string "name"
-    t.string "role"
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "name"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "data_drip_backfill_run_batches", "data_drip_backfill_runs", column: "backfill_run_id"
+  add_foreign_key "data_drip_backfill_run_batches",
+                  "data_drip_backfill_runs",
+                  column: "backfill_run_id"
 end
