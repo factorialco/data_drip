f195b33673d97fdb9f3187325fb14553f33ca447
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2026-02-17 09:25:00 +0200

    Fix rubocop errors

--- spec/helpers/data_drip/backfill_runs_helper_spec.rb
+++ spec/helpers/data_drip/backfill_runs_helper_spec.rb
@@ -19,11 +19,11 @@ RSpec.describe DataDrip::BackfillRunsHelper, type: :helper do
 
       it "renders checkbox as checked when default is true" do
         html = helper.backfill_option_inputs(backfill_run)
-        
+
         # Parse the HTML to check the checkbox state
         expect(html).to include('name="backfill_run[options][dry_run]"')
         expect(html).to include('type="checkbox"')
-        
+
         # The checkbox should be checked for dry_run (default: true)
         # In Rails, checked checkboxes have the 'checked' attribute
         dry_run_checkbox = html.match(/name="backfill_run\[options\]\[dry_run\]"[^>]*/)
@@ -32,10 +32,10 @@ RSpec.describe DataDrip::BackfillRunsHelper, type: :helper do
 
       it "renders checkbox as unchecked when default is false" do
         html = helper.backfill_option_inputs(backfill_run)
-        
+
         # The checkbox for verbose (default: false) should not be checked
         expect(html).to include('name="backfill_run[options][verbose]"')
-        
+
         # Extract the verbose checkbox HTML
         verbose_section = html.match(/name="backfill_run\[options\]\[verbose\]"[^>]*/)
         expect(verbose_section.to_s).not_to include('checked')
@@ -43,7 +43,7 @@ RSpec.describe DataDrip::BackfillRunsHelper, type: :helper do
 
       it "uses default value for text fields" do
         html = helper.backfill_option_inputs(backfill_run)
-        
+
         # Text field should have the default value
         expect(html).to include('name="backfill_run[options][name]"')
         expect(html).to include('value="default_name"')
@@ -63,7 +63,7 @@ RSpec.describe DataDrip::BackfillRunsHelper, type: :helper do
 
       it "still works for backfills without default values" do
         html = helper.backfill_option_inputs(backfill_run)
-        
+
         expect(html).to include('name="backfill_run[options][age]"')
         expect(html).to include('name="backfill_run[options][name]"')
       end
